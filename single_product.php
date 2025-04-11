150d3a717d44952e452736e7f93a2590a7d89ee5
hieu2603 <nguyenvanhieu2603@gmail.com>
2025-04-11 10:58:19 +0700

    feat: get single product

--- single_product.php
+++ single_product.php
@@ -1,3 +1,22 @@
+<?php
+
+include 'server/connection.php';
+
+if (isset($_GET['product_id'])) {
+  $product_id = $_GET['product_id'];
+
+  $stmt = $conn->prepare("SELECT * FROM products WHERE product_id = ?");
+  $stmt->bind_param("i", $product_id);
+
+  $stmt->execute();
+
+  $product = $stmt->get_result();
+} else {
+  header("location: index.php");
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -71,33 +90,36 @@
   <!-- Single product -->
   <section class="container single-product my-5 pt-5">
     <div class="row mt-5">
-      <div class="col-lg-5 col-md-6 col-sm-12">
-        <img src="assets/img/featured_1.jpg" alt="product_1_img" class="img-fluid w-100 pb-1" id="mainImg">
-        <div class="small-img-group">
-          <div class="small-img-col">
-            <img src="assets/img/featured_2.jpg" alt="featured_2_img" width="100%" class="small-img">
-          </div>
-          <div class="small-img-col">
-            <img src="assets/img/featured_3.jpg" alt="featured_2_img" width="100%" class="small-img">
-          </div>
-          <div class="small-img-col">
-            <img src="assets/img/featured_4.jpg" alt="featured_2_img" width="100%" class="small-img">
-          </div>
-          <div class="small-img-col">
-            <img src="assets/img/clothes_1.jpg" alt="featured_2_img" width="100%" class="small-img">
+      <?php while ($row = $product->fetch_assoc()) { ?>
+        <div class="col-lg-5 col-md-6 col-sm-12">
+          <img src="assets/img/<?php echo $row['product_image']; ?>" alt="product_1_img" class="img-fluid w-100 pb-1" id="mainImg">
+          <div class="small-img-group">
+            <div class="small-img-col">
+              <img src="assets/img/<?php echo $row['product_image']; ?>" alt="featured_2_img" width="100%" class="small-img">
+            </div>
+            <div class="small-img-col">
+              <img src="assets/img/<?php echo $row['product_image2']; ?>" alt="featured_2_img" width="100%" class="small-img">
+            </div>
+            <div class="small-img-col">
+              <img src="assets/img/<?php echo $row['product_image3']; ?>" alt="featured_2_img" width="100%" class="small-img">
+            </div>
+            <div class="small-img-col">
+              <img src="assets/img/<?php echo $row['product_image4']; ?>" alt="featured_2_img" width="100%" class="small-img">
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-lg-6 col-md-12 col-sm-12">
-        <h6>Men/Shoes</h6>
-        <h3 class="py-4">Men's Fashion</h3>
-        <h2>$69</h2>
-        <input type="number" value="1">
-        <button class="buy-btn">Add To Cart</button>
-        <h4 class="mt-5 mb-5">Product details</h4>
-        <span>The details of this product will be displayed shortly.</span>
-      </div>
+
+        <div class="col-lg-6 col-md-12 col-sm-12">
+          <h6>Men/Shoes</h6>
+          <h3 class="py-4"><?php echo $row['product_name']; ?></h3>
+          <h2><?php echo $row['product_price']; ?></h2>
+          <input type="number" value="1">
+          <button class="buy-btn">Add To Cart</button>
+          <h4 class="mt-5 mb-5">Product details</h4>
+          <span><?php echo $row['product_description']; ?></span>
+        </div>
+      <?php } ?>
     </div>
   </section>
 
@@ -234,11 +256,10 @@
     const smallImg = document.getElementsByClassName("small-img");
 
     for (let i = 0; i < 4; i++) {
-      smallImg[i].onclick = function () {
+      smallImg[i].onclick = function() {
         mainImg.src = smallImg[i].src;
       }
     }
-
   </script>
 </body>
 
